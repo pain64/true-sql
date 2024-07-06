f59185248b3172e0e7843166f517f3b11f4de5af
over <overln2@gmail.com>
2024-07-06 23:55:26 +0600

    in progress

--- lib/src/test/java/net/truej/sql/compiler/SuperTest.java
+++ lib/src/test/java/net/truej/sql/compiler/SuperTest.java
@@ -1,6 +1,7 @@
 package net.truej.sql.compiler;
 
 import net.truej.sql.TrueSql;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(TrueSqlTests.class)
 @TrueSql public class SuperTest {
+    @Disabled
     @Test public void test1(MainConnection cn) {
         cn.q("insert into t1 values(100, ?)", "haha")
             .asGeneratedKeys("id").fetchOne(Long.class);
