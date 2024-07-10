9a6857bbc22c1c4bac0855abe8692b0b727e87fb
over <overln2@gmail.com>
2024-07-10 17:18:02 +0600

    equals & hashCode for UpdateResult & UpdateResultStream

--- lib/src/main/java/net/truej/sql/fetch/UpdateResult.java
+++ lib/src/main/java/net/truej/sql/fetch/UpdateResult.java
@@ -1,5 +1,7 @@
 package net.truej.sql.fetch;
 
+import java.util.Objects;
+
 public class UpdateResult<U, V> {
     public final U updateCount;
     public final V value;
@@ -7,5 +9,19 @@ public class UpdateResult<U, V> {
     public UpdateResult(U updateCount, V value) {
         this.updateCount = updateCount;
         this.value = value;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        var that = (UpdateResult<?, ?>) o;
+        return updateCount.equals(that.updateCount) && Objects.equals(value, that.value);
+    }
+
+    @Override public int hashCode() {
+        var result = updateCount.hashCode();
+        result = 31 * result + Objects.hashCode(value);
+        return result;
     }
 }
--- lib/src/main/java/net/truej/sql/fetch/UpdateResultStream.java
+++ lib/src/main/java/net/truej/sql/fetch/UpdateResultStream.java
@@ -14,4 +14,18 @@ public class UpdateResultStream<U, V> implements AutoCloseable {
     @Override public void close() {
         value.close();
     }
+
+    @Override public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        var that = (UpdateResultStream<?, ?>) o;
+        return updateCount.equals(that.updateCount) && value.equals(that.value);
+    }
+
+    @Override public int hashCode() {
+        var result = updateCount.hashCode();
+        result = 31 * result + value.hashCode();
+        return result;
+    }
 }
