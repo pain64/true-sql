3031fafe51ff71df14fc4acade1271a38e175270
over <overln2@gmail.com>
2024-12-30 02:41:30 +0600

    workaround for jacoco

--- lib/src/main/java/net/truej/sql/fetch/MissConfigurationException.java
+++ lib/src/main/java/net/truej/sql/fetch/MissConfigurationException.java
@@ -1,10 +1,13 @@
 package net.truej.sql.fetch;
 
 class MissConfigurationException extends RuntimeException {
+    static boolean isJacocoWorkaroundEnabled = false;
 
     MissConfigurationException(String message) { super(message); }
 
     static <T> T raise() {
+        if (isJacocoWorkaroundEnabled) return null;
+
         throw new MissConfigurationException(
             "TrueSql compiler plugin not enabled. Check out your build tool configuration" +
             " (Gradle, Maven, ...)"
--- lib/src/test/java/net/truej/sql/fetch/__25__TrueSqlMissConfiguration.java
+++ lib/src/test/java/net/truej/sql/fetch/__25__TrueSqlMissConfiguration.java
@@ -1,177 +1,75 @@
 package net.truej.sql.fetch;
 
+import net.truej.sql.bindings.Standard;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 import static net.truej.sql.fetch.Parameters.*;
 
 public class __25__TrueSqlMissConfiguration {
-    class A implements As {}
+    class A implements As { }
     class B implements NoUpdateCount, NoUpdateCount.None, NoUpdateCount.OneG,
         NoUpdateCount.One, NoUpdateCount.OneOrZero, NoUpdateCount.OneOrZeroG, NoUpdateCount.ListG,
-        NoUpdateCount.List_, NoUpdateCount.StreamG, NoUpdateCount.Stream_ {}
+        NoUpdateCount.List_, NoUpdateCount.StreamG, NoUpdateCount.Stream_ { }
     class C implements UpdateCount, UpdateCount.None, UpdateCount.OneG,
         UpdateCount.One, UpdateCount.OneOrZero, UpdateCount.OneOrZeroG, UpdateCount.ListG,
-        UpdateCount.List_, UpdateCount.StreamG, UpdateCount.Stream_ {}
-    
-    @Test
-    void test() {
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> out(Integer.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> inout(1)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> unfold(new ArrayList<>() )
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> unfold(new ArrayList<Integer>(), (x) -> new Object[]{x} )
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> inout(1)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new Q<>() {}.q("hello")
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new Q<>() {}.q(List.of(), "hello", v -> new Object[]{})
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new A().asCall()
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new A().asGeneratedKeys("id")
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new A().asGeneratedKeys(1)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new B().fetchNone()
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new B().fetchOne(String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new B().fetchOne(String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new B().fetchOne(Nullable, String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new B().fetchOne(NotNull, String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new B().fetchOneOrZero(String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new B().fetchOneOrZero(NotNull, String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new B().fetchOneOrZero(Nullable, String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new B().fetchList(String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new B().fetchList(NotNull, String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new B().fetchList(Nullable, String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new B().fetchStream(String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new B().fetchStream(NotNull, String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new B().fetchStream(Nullable, String.class)
-        );
+        UpdateCount.List_, UpdateCount.StreamG, UpdateCount.Stream_ { }
 
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new C().fetchNone()
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new C().fetchOne(String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new C().fetchOne(String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new C().fetchOne(Nullable, String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new C().fetchOne(NotNull, String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new C().fetchOneOrZero(String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new C().fetchOneOrZero(NotNull, String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new C().fetchOneOrZero(Nullable, String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new C().fetchList(String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new C().fetchList(NotNull, String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new C().fetchList(Nullable, String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new C().fetchStream(String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
-            () -> new C().fetchStream(NotNull, String.class)
-        );
-        Assertions.assertThrows(
-            MissConfigurationException.class,
+
+    @Test void test() throws Throwable {
+        new Parameters(); // for Jacoco
+        new Standard(); // for Jacoco
+
+        var actions = List.<Executable>of(
+            () -> out(Integer.class),
+            () -> inout(1),
+            () -> unfold(new ArrayList<>()),
+            () -> unfold(new ArrayList<Integer>(), (x) -> new Object[]{x}),
+            () -> inout(1),
+            () -> new Q<>() { }.q("hello"),
+            () -> new Q<>() { }.q(List.of(), "hello", v -> new Object[]{}),
+            () -> new A().asCall(),
+            () -> new A().asGeneratedKeys("id"),
+            () -> new A().asGeneratedKeys(1),
+            () -> new B().fetchNone(),
+            () -> new B().fetchOne(String.class),
+            () -> new B().fetchOne(String.class),
+            () -> new B().fetchOne(Nullable, String.class),
+            () -> new B().fetchOne(NotNull, String.class),
+            () -> new B().fetchOneOrZero(String.class),
+            () -> new B().fetchOneOrZero(NotNull, String.class),
+            () -> new B().fetchOneOrZero(Nullable, String.class),
+            () -> new B().fetchList(String.class),
+            () -> new B().fetchList(NotNull, String.class),
+            () -> new B().fetchList(Nullable, String.class),
+            () -> new B().fetchStream(String.class),
+            () -> new B().fetchStream(NotNull, String.class),
+            () -> new B().fetchStream(Nullable, String.class),
+            () -> new C().fetchNone(),
+            () -> new C().fetchOne(String.class),
+            () -> new C().fetchOne(Nullable, String.class),
+            () -> new C().fetchOne(NotNull, String.class),
+            () -> new C().fetchOneOrZero(String.class),
+            () -> new C().fetchOneOrZero(NotNull, String.class),
+            () -> new C().fetchOneOrZero(Nullable, String.class),
+            () -> new C().fetchList(String.class),
+            () -> new C().fetchList(NotNull, String.class),
+            () -> new C().fetchList(Nullable, String.class),
+            () -> new C().fetchStream(String.class),
+            () -> new C().fetchStream(NotNull, String.class),
             () -> new C().fetchStream(Nullable, String.class)
         );
 
+        for (var action : actions)
+            Assertions.assertThrows(MissConfigurationException.class, action);
+
+        MissConfigurationException.isJacocoWorkaroundEnabled = true;
+        for (var action : actions)
+            action.execute();
     }
 }
